319d06239e4ae63a44ca73579211fcbb7fe3644b
Clapinton <eric.olivetree@gmail.com>
2017-01-26 22:14:04 -0800

    Add console.log instructions for jQuery and React

--- frontend/acme-finghi.jsx
+++ frontend/acme-finghi.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import SearchSpecies from './components/search/search';
 
 document.addEventListener("DOMContentLoaded", () => {
+  console.log("This interface was built with React and JS ES6. Add ?render=jquery to the URL for the jQuery version.")
   const mainEl = document.getElementById("react-container");
 
   ReactDOM.render(<SearchSpecies />, mainEl);
